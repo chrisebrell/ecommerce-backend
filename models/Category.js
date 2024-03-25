446fe7c928b075a3eabdfb7c643944a08f918514
Bhuvaneswari630 <bhuvaneswarim630@gmail.com>
2024-03-25 19:44:07 -0400

    added imageUrl field to category model

--- models/Category.js
+++ models/Category.js
@@ -4,6 +4,7 @@ const CategorySchema = new Schema({
   name: { type: String, required: true },
   parent: { type: mongoose.Types.ObjectId, ref: "Category" },
   properties: [{ type: Object }],
+  imageUrl: {type: String}
 });
 
 export const Category = models?.Category || model("Category", CategorySchema);
